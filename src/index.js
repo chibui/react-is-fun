a86470c167de875d1362fefe3c48bd0bc378d425
Chi Bui <chi.bui@tal.com.au>
2020-11-24 14:56:29 +1100

    component properties

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDom from 'react-dom';
 
 class Message extends React.Component {
     render() {
+        console.log('this.props', this.props);
+        
         return (
             <div>
-                <h1>Hello Everyone</h1>
+                <h1 style={{ color: this.props.color }}>{ this.props.msg }</h1>
+                <p>I'll check back in { this.props.minutes } minutes</p>
             </div>
         )
     }
 };
 
-ReactDom.render(<Message />, document.getElementById('root'));
+ReactDom.render(
+    <Message color="blue" msg="how are you?" minutes={5}/>, 
+    document.getElementById('root')
+);
