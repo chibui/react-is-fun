c7c6dfa1b995ff9d8d1135f261c8fbeb1254d553
Chi Bui <chi_bui@outlook.com>
2020-12-29 09:07:12 +1100

    refactor as function component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
 let skiData = {
@@ -8,40 +8,31 @@ let skiData = {
     goal: 100
 }
 
-class SkiDayCounter extends Component{
-    getPercent = decimal => {
+    const getPercent = decimal => {
         return decimal * 100 + '%';
     }
 
-    calcGoalProgress = (total, goal) => {
-        return this.getPercent(total/goal);
+    const calcGoalProgress = (total, goal) => {
+        return getPercent(total/goal);
     }
 
-    render() {
-        const {
-            backCountry,
-            goal,
-            powder,
-            total
-        } = this.props;
-
-        return (
-            <section>
-                <div>
-                    <p>Total Days: {total}</p>
-                </div>
-                <div>
-                    <p>Powder Days: {powder}</p>
-                </div>
-                <div>
-                    <p>Back Country Days: {backCountry}</p>
-                </div>
-                <div>
-                    <p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
-                </div>
-            </section>
-        )
-    }
+const SkiDayCounter = ({backCountry, goal, powder, total}) => {
+    return (
+        <section>
+            <div>
+                <p>Total Days: { total }</p>
+            </div>
+            <div>
+                <p>Powder Days: { powder }</p>
+            </div>
+            <div>
+                <p>Back Country Days: { backCountry }</p>
+            </div>
+            <div>
+                <p>Goal Progress: { calcGoalProgress(total, goal) }</p>
+            </div>
+        </section>
+    )
 }
 
 render(
